58acddf76969e538d629d263aa971382bf2ff024
NorthDice <mister.balychev@gmail.com>
2025-04-12 19:22:44 +0300

    Unfinished third part(just no need)

---
+++ LogStory/pocketLog/level.go
@@ -0,0 +1,15 @@
+package main
+
+// Level represents a log level
+type Level byte
+
+const (
+	//LevelDebug is the debug log level
+	LevelDebug Level = iota
+
+	//LevelInfo is the info log level
+	LevelInfo
+
+	//LevelError is the error log level
+	LevelError
+)
